d294d3d9f925dabaf905b520cf7d68a81f2d98e3
t-setohi <t-setohi@MININT-FBSIUD5>
2017-01-16 13:05:10 -0800

    Added the over9000 message

--- first.js
+++ first.js
@@ -1,7 +1,13 @@
 MAX_NUMBER = 10000
 var http = require('http');
-var pwrlvl = function () { return Math.floor((Math.random() * MAX_NUMBER) + 1) };
-var port = process.env.PORT || 300
+var pwrlvl = function () {
+    var result = Math.floor((Math.random() * MAX_NUMBER) + 1);
+    if (result > 9000) {
+        result += " IT'S OVER 9000!!!!!";
+    }
+    return result
+};
+var port = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
