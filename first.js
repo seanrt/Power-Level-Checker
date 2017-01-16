16b016f805eb37933bdbb716f0c97ba1ba640fde
t-setohi <t-setohi@MININT-FBSIUD5>
2017-01-16 11:14:44 -0800

    Added package.json

--- first.js
+++ first.js
@@ -1,10 +1,11 @@
 MAX_NUMBER = 10000
 var http = require('http');
 var pwrlvl = function () { return Math.floor((Math.random() * MAX_NUMBER) + 1) };
+var port = process.env.PORT || 300
 
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Your power level is: '+pwrlvl()+'\n');
-}).listen(8080);
+}).listen(port);
 
 console.log('Server started');
